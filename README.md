535ad6e3984816dba3a939d1b1d42aa2d820b7f1
钟晓庆 <zhongxiaoqing@tungee.com>
2017-07-14 19:26:02 +0800

    Update

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 1. Head -> 16 bits(x, y)连接着Body -> 16 bit (word)(x, y)
 2. si -> 地址数组,用来暂时储存
-3. print ?
+3. print ??
+4. int16 -> 键盘I/O  int 10 -> 视频I/O
--- snake.asm
+++ snake.asm
@@ -59,9 +59,9 @@ done_update:
     add	si, 2		; 蛇长大了
 add_zero_snake:
     mov	word [si], 0x0000 ;
-print_stuff:
-    xor	dx, dx		; set pos to 0x0000
-    call move_cursor	; move cursor
+print_stuff: ; 打印界面
+    xor	dx, dx		; 显存控制寄存器置零
+    call move_cursor	;
     mov	si, score_msg	; prepare to print score string
     call	print_string 	; print it
     mov	ax, [score]	; move the score into ax
@@ -76,139 +76,141 @@ print_stuff:
     call	print_char	; print it
     mov	si, snake_body_pos ; prepare to print snake body
 snake_body_print_loop:
-lodsw			; load position from the body, and increment si
-test	ax, ax		; check if position is zero
-jz	check_collisions ; if it was zero, move out of here
-mov	dx, ax		; if not, move the position into dx
-call	move_cursor	; move the cursor there
-mov	al, 'o'		; use 'o' as the snake body symbol
-call	print_char	; print it
-jmp	snake_body_print_loop ; loop
-
+    lodsw			; load position from the body, and increment si
+    test	ax, ax		; check if position is zero
+    jz	check_collisions ; if it was zero, move out of here
+    mov	dx, ax		; if not, move the position into dx
+    call	move_cursor	; move the cursor there
+    mov	al, 'o'		; use 'o' as the snake body symbol
+    call	print_char	; print it
+    jmp	snake_body_print_loop ; loop
 check_collisions:
-mov	bx, [snake_pos]	; move the snake head position into bx
-cmp	bh, 25		; check if we are too far down
-jge	game_over_hit_wall ; if yes, jump
-cmp	bh, 0		; check if we are too far up
-jl	game_over_hit_wall ; if yes, jump
-cmp	bl, 80 ; check if we are too far to the right
-jge	game_over_hit_wall ; if yes, jump
-cmp	bl, 0		; check if we are too far to the left
-jl	game_over_hit_wall ; if yes, jump
-mov	si, snake_body_pos ; prepare to check for self-collision
-check_collisions_self:
-lodsw			; load position of snake body, and increment si
-cmp	ax, bx		; check if head position = body position
-je	game_over_hit_self ; if it is, jump
-or	ax, ax		; check if position is 0x0000 (we are done searching)
-jne	check_collisions_self ; if not, loop
-
+    mov	bx, [snake_pos]	; 将蛇头位置储存在Bx
+    cmp	bh, 25		; 判断是否撞到墙（下面）
+    jge	game_over_hit_wall
+    cmp	bh, 0		; 判断是否撞到墙（上面）
+    jl	game_over_hit_wall
+    cmp	bl, 80 ; 判断是否撞到墙（右面）
+    jge	game_over_hit_wall
+    cmp	bl, 0		; 判断是否撞到墙（左面）
+    jl	game_over_hit_wall
+    mov	si, snake_body_pos ; 加载蛇身位置
+check_collisions_self: ; 跌代判断蛇身与蛇头位置是否相等
+    lodsw  ; 加载蛇身位置并将si ++2
+    cmp	ax, bx
+    je	game_over_hit_self
+    or	ax, ax     ; 判断是否到达蛇尾，即蛇自撞检测结束条件
+    jne	check_collisions_self ; 没则继续检测
 no_collision:
-mov	ax, [snake_pos]	; load snake head position into ax
-cmp	ax, [food_pos]	; check if we are on the food
-jne	game_loop_continued ; jump if snake didn't hit food
-inc	word [score]	; if we were on food, increment score
-mov	bx, 24		; set max value for random call (y-val - 1)
-call	rand		; generate random value
-push	dx		; save it on the stack
-mov	bx, 78 ; set max value for random call
-call	rand		; generate random value
-pop	cx		; restore old random into cx
-mov	dh, cl		; move old value into high bits of new
-mov	[food_pos], dx	; save the position of the new random food
-mov	byte [grow_snake_flag], 1 ; make sure snake grows
+    mov	ax, [snake_pos]	; 加载蛇头位置
+    cmp	ax, [food_pos]	; 与食物位置判断是否吃到
+    jne	game_loop_continued ; 如果没吃到，则直接跳转到game_loop_continued
+    inc	word [score]	; 计分器++1
+    mov	bx, 24		; 初始化行随机数范围
+    call rand		; 调用随机函数结果储存在dx
+    push dx		; 将 xpos（dx）压栈保存
+    mov	bx, 78 ; 初始化列随机数范围
+    call rand		; 产生随机数ypos（dx）
+    pop	cx		; 将行位置出栈于cx
+    mov	dh, cl		; 保存cl（实际的行位置）
+    mov	[food_pos], dx	; 更新食物位置
+    mov	byte [grow_snake_flag], 1 ; 标志变量grow_snake_flag置1
 game_loop_continued:
-mov	cx, 0x0002	; Sleep for 0,15 seconds (cx:dx)
-mov	dx, 0x49F0	; 0x000249F0 = 150000
-mov	ah, 0x86
-int	0x15		; Sleep
-jmp	game_loop	; loop
+    mov	cx, 0x0002	; Sleep for 0,15 seconds (cx:dx)
+    mov	dx, 0x49F0	; 0x000249F0 = 150000
+    mov	ah, 0x86
+    int	0x15		; Sleep
+    jmp	game_loop	; loop
 
 game_over_hit_self:
-push 	self_msg
-jmp	game_over
+    push 	self_msg
+    jmp	game_over
 
 game_over_hit_wall:
-push	wall_msg
+    push	wall_msg
 
 game_over:
-call	cls
-mov	si, hit_msg
-call	print_string
-pop	si
-call	print_string
-mov	si, retry_msg
-call	print_string
+    call cls
+    mov	si, hit_msg
+    call	print_string
+    pop	si
+    call	print_string
+    mov	si, retry_msg
+    call	print_string
 wait_for_r:
-mov	ah, 0x00
-int	0x16
-cmp	al, 'r'
-jne	wait_for_r
-mov	word [snake_pos], 0x0F0F
-and	word [snake_body_pos], 0
-and	word [score], 0
-mov	byte [last_move], 'd'
-jmp	game_loop
+    mov	ah, 0x00
+    int	0x16
+    cmp	al, 'r'
+    jne	wait_for_r
+    mov	word [snake_pos], 0x0F0F
+    and	word [snake_body_pos], 0
+    and	word [score], 0
+    mov	byte [last_move], 'd'
+    jmp	game_loop
 
-; SCREEN FUNCTIONS ------------------------------------------------------------
+; 屏幕功能区 ------------------------------------------------------------
 cls:
-mov	ax, 0x0700	; clear entire window (ah 0x07, al 0x00)
-mov	bh, 0x0C	; light red on black
-xor	cx, cx		; top left = (0,0)
-mov	dx, 0x1950	; bottom right = (25, 80)
-int	0x10
-xor	dx, dx		; set dx to 0x0000
-call	move_cursor	; move cursor
-ret
+    mov	ax, 0x0700	; clear entire window (ah 0x07, al 0x00)
+    mov	bh, 0x0C	; light red on black
+    xor	cx, cx		; top left = (0,0)
+    mov	dx, 0x1950	; bottom right = (25, 80)
+    int	0x10
+    xor	dx, dx		; set dx to 0x0000
+    call	move_cursor	; move cursor
+    ret
 
 move_cursor:
-mov	ah, 0x02	; move to (dl, dh)
-xor	bh, bh		; page 0
-int 	0x10
-ret
+    mov	ah, 0x02	; move to (dl, dh)
+    xor	bh, bh		; page 0
+    int 	0x10
+    ret
 
 print_string_loop:
-call print_char
+    call print_char
 print_string:			; print the string pointed to in si
-lodsb			; load next byte from si
-test	al, al		; check if high bit is set (end of string)
-jns	print_string_loop	; loop if high bit was not set
+    lodsb			; load next byte from si
+    test	al, al		; check if high bit is set (end of string)
+    jns	print_string_loop	; loop if high bit was not set
 
 print_char:			; print the char at al
-and	al, 0x7F	; unset the high bit
-mov	ah, 0x0E
-int	0x10
-ret
+    and	al, 0x7F	; unset the high bit
+    mov	ah, 0x0E
+    int	0x10
+    ret
 
 print_int:			; print the int in ax
-push	bp		; save bp on the stack
-mov	bp, sp		; set bp = stack pointer
+    push	bp		; save bp on the stack
+    mov	bp, sp		; set bp = stack pointer
 
 push_digits:
-xor	dx, dx		; clear dx for division
-mov	bx, 10		; set bx to 10
-div	bx		; divide by 10
-push	dx		; store remainder on stack
-test	ax, ax		; check if quotient is 0
-jnz 	push_digits	; if not, loop
+    xor	dx, dx		; clear dx for division
+    mov	bx, 10		; set bx to 10
+    div	bx		; divide by 10
+    push	dx		; store remainder on stack
+    test	ax, ax		; check if quotient is 0
+    jnz 	push_digits	; if not, loop
 
 pop_and_print_digits:
-pop	ax		; get first digit from stack
-add	al, '0'		; turn it into ascii digits
-call	print_char	; print it
-cmp	sp, bp		; is the stack pointer is at where we began?
-jne	pop_and_print_digits ; if not, loop
-pop	bp		; if yes, restore bp
-ret
-; UTILITY FUNCTIONS -----------------------------------------------------------
-rand:				; random number between 1 and bx. result in dx
-mov	ah, 0x00
-int	0x1A		; get clock ticks since midnight
-mov	ax, dx		; move lower bits into ax for division
-xor	dx, dx		; clear dx
-div	bx		; divide ax by bx to get remainder in dx
-inc	dx
-ret
+    pop	ax		; get first digit from stack
+    add	al, '0'		; turn it into ascii digits
+    call	print_char	; print it
+    cmp	sp, bp		; is the stack pointer is at where we began?
+    jne	pop_and_print_digits ; if not, loop
+    pop	bp		; if yes, restore bp
+    ret
+
+;--------------------------------------------------------------------
+;  函数
+;--------------------------------------------------------------------
+; 随机函数， 利用功能号为10的1A中断（）产生1至bx的随机数 -> dx
+rand:
+    mov	ah, 0x00
+    int	0x1A		; get clock ticks since midnight
+    mov	ax, dx		; move lower bits into ax for division
+    xor	dx, dx		; clear dx
+    div	bx		; divide ax by bx to get remainder in dx
+    inc	dx
+    ret
 
 ; ------------------------------------------------------------------------
 ;   定义消息,变量以及变量初始化
