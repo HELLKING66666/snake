1b5001663ce46d91301cc7f12b5e85651af7f97b
钟晓庆 <zhongxiaoqing@tungee.com>
2017-07-16 16:35:34 +0800

    a

--- snake.asm
+++ snake.asm
@@ -1,12 +1,39 @@
-mov	ax, 0x07C0
-mov	ds, ax		; 加载ds到ax
+org 0x100
+mov ax, cs
+mov es, ax
+mov ds, ax
+mov ss, ax  ; 设置堆栈指针
+mov sp, 100h - 8
+; 环境设置
 mov	ah, 0x01
 mov	cx, 0x2000
-; 环境设置
 int 0x10		; 利用10号中断停止光标闪烁
 mov	ax, 0x0305
 mov	bx, 0x031F
 int	0x16 ; 增加重复键入延迟
+call game_loop
+
+
+;------------------------------------------------------------------------
+;  定义消息,变量以及变量初始化
+;------------------------------------------------------------------------
+Scostr db 'Score: '
+Scostrlen equ $ - Scostr
+HitSelfstr db 'You hit yourself ! Press r to retry'
+HitSelfstrlen equ $ - HitSelfstr
+HitWallstr db 'You hit the wall ! Press r to retry'
+HitWallstrlen equ $ - HitWallstr
+
+; 变量以及初始化
+grow_snake_flag db 0
+food_pos dw 0x0D0D
+score dw 0
+last_move db 'd'
+snake_pos:
+    snake_x_pos db 0x0F
+    snake_y_pos db 0x0F
+    snake_body_pos dw 0x0000
+
 
 game_loop:
     call cls ; 清屏
@@ -60,30 +87,27 @@ done_update:
 add_zero_snake:
     mov	word [si], 0x0000 ;
 print_stuff: ; 打印界面
-    xor	dx, dx		; 显存控制寄存器置零
-    call move_cursor	;
-    mov	si, score_msg	; prepare to print score string
-    call	print_string 	; print it
-    mov	ax, [score]	; move the score into ax
-    call	print_int	; print it
-    mov	dx, [food_pos] 	; set dx to the food position
-    call	move_cursor	; move cursor there
-    mov	al, '*'		; use '*' as food symbol
-    call	print_char	; print food
-    mov	dx, [snake_pos]	; set dx to the snake head position
-    call	move_cursor	; move there
-    mov	al, '@'		; use '@' as snake head symbol
-    call	print_char	; print it
-    mov	si, snake_body_pos ; prepare to print snake body
+    call DispScoStr ; 打印分数段
+    mov	ax, [score]	; 将Score传入寄存器ax，准备调用
+    call print_int	; 打印数字
+    mov	dx, [food_pos] 	; 传入食物位置，准备移动光标
+    call move_cursor	; 移动光标
+    mov	al, '*'		; 食物'*'传入al
+    call print_char	; 打印食物
+    mov	dx, [snake_pos]	; 传入蛇头位置，准备移动光标
+    call	move_cursor	; 移动光标
+    mov	al, '@'		; 打印蛇头
+    call	print_char	; 打印蛇头
+    mov	si, snake_body_pos ; 传入蛇身数组位置，准备打印蛇身
 snake_body_print_loop:
-    lodsw			; load position from the body, and increment si
-    test	ax, ax		; check if position is zero
-    jz	check_collisions ; if it was zero, move out of here
-    mov	dx, ax		; if not, move the position into dx
-    call	move_cursor	; move the cursor there
-    mov	al, 'o'		; use 'o' as the snake body symbol
-    call	print_char	; print it
-    jmp	snake_body_print_loop ; loop
+    lodsw			; 存取串操作lodsw（字）：AX ← [DS:(R|E)SI]、(R|E)SI ← (R|E)SI ± 2
+    test	ax, ax		; 判断是够蛇身存在
+    jz	check_collisions ; 蛇身没有则跳转到check_collisions
+    mov	dx, ax		; 传入蛇身位置，准备移动光标到蛇身位置
+    call	move_cursor	; 移动光标
+    mov	al, 'o'		; 蛇身标志为'o'
+    call	print_char	; 打印蛇身
+    jmp	snake_body_print_loop ; 迭代操作
 check_collisions:
     mov	bx, [snake_pos]	; 将蛇头位置储存在Bx
     cmp	bh, 25		; 判断是否撞到墙（下面）
@@ -123,41 +147,75 @@ game_loop_continued:
     jmp	game_loop	; loop
 
 game_over_hit_self:
-    push 	self_msg
-    jmp	game_over
+    call cls
+    call DispHitSelfStr
+    call wait_for_r
 
 game_over_hit_wall:
-    push	wall_msg
-
-game_over:
     call cls
-    mov	si, hit_msg
-    call	print_string
-    pop	si
-    call	print_string
-    mov	si, retry_msg
-    call	print_string
+    call DispHitWallStr
+    call wait_for_r
+
 wait_for_r:
     mov	ah, 0x00
     int	0x16
     cmp	al, 'r'
-    jne	wait_for_r
+    jne	goout
     mov	word [snake_pos], 0x0F0F
     and	word [snake_body_pos], 0
     and	word [score], 0
     mov	byte [last_move], 'd'
     jmp	game_loop
 
+goout:
+    mov ax,4c00h    ; AH=4Ch（功能号，终止进程）、AL=0（返回代码）
+    int 21h         ; DOS软中断
+
 ; 屏幕功能区 ------------------------------------------------------------
 cls:
-    mov	ax, 0x0700	; clear entire window (ah 0x07, al 0x00)
-    mov	bh, 0x0C	; light red on black
-    xor	cx, cx		; top left = (0,0)
-    mov	dx, 0x1950	; bottom right = (25, 80)
-    int	0x10
-    xor	dx, dx		; set dx to 0x0000
-    call	move_cursor	; move cursor
-    ret
+	mov ah, 06h     ; 功能号(向上滚动文本显示屏幕)
+	mov al, 0
+	mov ch, 0
+	mov cl, 0
+	mov dh, 24
+	mov dl, 79
+	mov bh, 0ch
+	int 10h ; 调用中断清屏
+	ret     ; 例程返回
+;
+DispScoStr: ; 显示分数字符串例程
+	mov ah, 13h 	; BIOS中断的功能号（显示字符串）
+	mov al, 1 		; 光标放到串尾
+	mov bh, 0 		; 页号 = 0
+	mov bl, 0ch 	; 字符颜色=不闪（0）黑底（000）亮红字（1100）
+	mov cx, Scostrlen; 串长=strlen
+	mov dx, 0 		; 显示串的起始位置（0，0）：DH=行号、DL=列号
+	mov bp, Scostr; ES:BP=串地址
+	int 10h 		; 调用10H号显示中断
+	ret				; 从例程返回
+
+DispHitSelfStr: ; 显示自杀字符串例程
+	mov ah, 13h 	; BIOS中断的功能号（显示字符串）
+	mov al, 1 		; 光标放到串尾
+	mov bh, 0 		; 页号 = 0
+	mov bl, 0ch 	; 字符颜色=不闪（0）黑底（000）亮红字（1100）
+	mov cx, HitSelfstrlen; 串长=strlen
+	mov dx, 0 		; 显示串的起始位置（0，0）：DH=行号、DL=列号
+	mov bp, HitSelfstr; ES:BP=串地址
+	int 10h 		; 调用10H号显示中断
+	ret				; 从例程返回
+
+DispHitWallStr: ; 显示撞墙字符串例程
+	mov ah, 13h 	; BIOS中断的功能号（显示字符串）
+	mov al, 1 		; 光标放到串尾
+	mov bh, 0 		; 页号 = 0
+	mov bl, 0ch 	; 字符颜色=不闪（0）黑底（000）亮红字（1100）
+	mov cx, HitWallstrlen; 串长=strlen
+	mov dx, 0 		; 显示串的起始位置（0，0）：DH=行号、DL=列号
+	mov bp, HitWallstr; ES:BP=串地址
+	int 10h 		; 调用10H号显示中断
+	ret				; 从例程返回
+
 
 move_cursor:
     mov	ah, 0x02	; move to (dl, dh)
@@ -165,38 +223,34 @@ move_cursor:
     int 	0x10
     ret
 
-print_string_loop:
-    call print_char
-print_string:			; print the string pointed to in si
-    lodsb			; load next byte from si
-    test	al, al		; check if high bit is set (end of string)
-    jns	print_string_loop	; loop if high bit was not set
-
 print_char:			; print the char at al
     and	al, 0x7F	; unset the high bit
     mov	ah, 0x0E
     int	0x10
     ret
 
-print_int:			; print the int in ax
-    push	bp		; save bp on the stack
-    mov	bp, sp		; set bp = stack pointer
+;-------------------------------------------------------------------------
+;  打印数字例程，由print_int,push_digits,pop_and_print_digits共同完成
+;-------------------------------------------------------------------------
+print_int:			; 参数为ax
+    push bp		;
+    mov	bp, sp		;
 
 push_digits:
-    xor	dx, dx		; clear dx for division
-    mov	bx, 10		; set bx to 10
-    div	bx		; divide by 10
-    push	dx		; store remainder on stack
-    test	ax, ax		; check if quotient is 0
-    jnz 	push_digits	; if not, loop
+    xor	dx, dx
+    mov	bx, 10
+    div	bx
+    push dx
+    test ax, ax
+    jnz push_digits
 
 pop_and_print_digits:
-    pop	ax		; get first digit from stack
-    add	al, '0'		; turn it into ascii digits
-    call	print_char	; print it
-    cmp	sp, bp		; is the stack pointer is at where we began?
-    jne	pop_and_print_digits ; if not, loop
-    pop	bp		; if yes, restore bp
+    pop	ax
+    add	al, '0'
+    call print_char
+    cmp	sp, bp
+    jne	pop_and_print_digits
+    pop	bp
     ret
 
 ;--------------------------------------------------------------------
@@ -205,34 +259,9 @@ pop_and_print_digits:
 ; 随机函数， 利用功能号为10的1A中断（）产生1至bx的随机数 -> dx
 rand:
     mov	ah, 0x00
-    int	0x1A		; get clock ticks since midnight
+    int	0x1A	   	; get clock ticks since midnight
     mov	ax, dx		; move lower bits into ax for division
     xor	dx, dx		; clear dx
     div	bx		; divide ax by bx to get remainder in dx
     inc	dx
     ret
-
-; ------------------------------------------------------------------------
-;   定义消息,变量以及变量初始化
-; ------------------------------------------------------------------------
-; 消息MESSAGES (Encoded as 7-bit strings. Last byte is an ascii value with its
-retry_msg db '! press r to retr', 0xF9 ; y
-hit_msg db 'You hit', 0xA0 ; space
-self_msg db 'yoursel', 0xE6 ; f
-wall_msg db 'the wal', 0xEC ; l
-score_msg db 'Score:', 0xA0 ; space
-
-; 变量以及初始化
-grow_snake_flag db 0
-food_pos dw 0x0D0D
-score dw 1
-last_move db 'd'
-snake_pos:
-snake_x_pos db 0x0F
-snake_y_pos db 0x0F
-snake_body_pos dw 0x0000
-
-; PADDING AND BOOT SIGNATURE --------------------------------------------------
-times 510-($-$$) db 0
-db 0x55
-db 0xAA
